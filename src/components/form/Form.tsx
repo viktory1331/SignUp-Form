5f25a3359d7593575138675f465be13dc29c8cba
viktory1331 <viktoriyafedosowa@gmail.com>
2021-08-21 01:51:38 +0300

    feat: add Gender component

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,12 +1,14 @@
 import SignUp from '../signUp/SignUp';
 import { ReactComponent as Logo } from '../../assets/image/logo.svg';
 import styles from './Form.module.scss';
+import Gender from '../gender/Gender';
 
 const Form = () => {
   return (
     <div className={styles.formContainer}>
       <h1 className={styles.textStyle}>Sign Up with email</h1>
       <Logo />
+      <Gender />
       <SignUp />
     </div>
   );
